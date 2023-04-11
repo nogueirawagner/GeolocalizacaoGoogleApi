563096867b6812216e72b0c7df7ef283c8569dc1
Wagner Nogueira <arquitetowagnernogueira@gmail.com>
2023-04-11 11:17:39 -0400

    ajustando responsivo de telas

--- GestaoDDD.MVC/Scripts/GoogleMaps/Candidatos/mapa_candidatos_proximos.js
+++ GestaoDDD.MVC/Scripts/GoogleMaps/Candidatos/mapa_candidatos_proximos.js
@@ -4,115 +4,6 @@ var marker;
 
 function initialize() {
 
-    var vbLat = document.getElementById("vbLat").value;
-    var vbLong = document.getElementById("vbLong").value;
-    var vbEndereco = document.getElementById("vbEndereco").value;
-
-    // Chama o serviço para pegar os dados
-    fetch('/Candidato/BuscaCandidatosProximosJson?Latitude=' + vbLat + '&Longitude=' + vbLong + '&Endereco' + vbEndereco)
-        .then(response => response.json())
-        .then(data => {
-            // Atualiza a variável locations com os dados retornados pelo serviço
-            var locations = data;
-
-            var directionsService = new google.maps.DirectionsService();
-            var directionsRenderer = new google.maps.DirectionsRenderer();
-
-            var cepomLat = -15.7994772;
-            var cepomLong = -48.0607247;
-
-            var espcLat = -15.8851054;
-            var espcLong = -48.0626557;
-
-            var cepomPoint = new google.maps.LatLng(cepomLat, cepomLong);
-            var espcPoint = new google.maps.LatLng(espcLat, espcLong);
-            var originPoint = new google.maps.LatLng(vbLat, vbLong);
-            var tempoDuracao = "";
-            var localCurso = "";
-
-            // Continua com o restante do código
-            var map = new google.maps.Map(document.getElementById('mapa'), {
-                zoom: 18,
-                center: new google.maps.LatLng(locations[0].Latitude, locations[0].Longitude),
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            });
-
-            //geocoder = new google.maps.Geocoder();
-
-            var infowindow = new google.maps.InfoWindow();
-
-            var marker, i;
-
-            const imageCursos =
-                "https://developers.google.com/maps/documentation/javascript/examples/full/images/parking_lot_maps.png";
-            // Localização do CEPOM
-            marker = new google.maps.Marker({
-                position: new google.maps.LatLng(cepomLat, cepomLong),
-                map: map,
-                title: "CEPOM",
-                icon: imageCursos
-            });
-
-            google.maps.event.addListener(marker, 'click', (function (marker) {
-                return function () {
-                    infowindow.setContent("CEPOM");
-                    infowindow.open(map, marker);
-                }
-            })(marker));
-
-            if (vbLat && vbLong) {
-                const image =
-                    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
-                // Local do cara
-                marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(vbLat, vbLong),
-                    map: map,
-                    title: locations[0].DistanciaColegas,
-                    icon: image
-                });
-
-                google.maps.event.addListener(marker, 'click', (function (marker) {
-                    return function () {
-                        infowindow.setContent("Meu local");
-                        infowindow.open(map, marker);
-                    }
-                })(marker));
-
-            }
-
-            // Localização da ESPC
-            marker = new google.maps.Marker({
-                position: new google.maps.LatLng(espcLat, espcLong),
-                map: map,
-                title: "ESPC",
-                icon: imageCursos
-            });
-
-            google.maps.event.addListener(marker, 'click', (function (marker) {
-                return function () {
-                    infowindow.setContent("ESPC");
-                    infowindow.open(map, marker);
-                }
-            })(marker));
-
-            for (i = 0; i < locations.length; i++) {
-                marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(locations[i].Latitude, locations[i].Longitude),
-                    map: map,
-                    title: locations[i].Nome
-                });
-
-                google.maps.event.addListener(marker, 'click', (function (marker, i) {
-                    return function () {
-                        infowindow.setContent('Nome: ' + locations[i].Nome + ' ' + 'Telefone: ' + locations[i].Telefone);
-                        infowindow.open(map, marker);
-                    }
-                })(marker, i));
-            }
-            directionsRenderer.setMap(map);
-        });
-
-
 }
 
 $(document).ready(function () {
@@ -140,7 +31,7 @@ $(document).ready(function () {
 
                             // Continua com o restante do código
                             var map = new google.maps.Map(document.getElementById('mapa'), {
-                                zoom: 14,
+                                zoom: 13,
                                 center: new google.maps.LatLng(vbLat, vbLong),
                                 mapTypeId: google.maps.MapTypeId.ROADMAP
                             });
