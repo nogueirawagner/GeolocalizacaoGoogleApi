01e996670785a32818cbefd1d3d1d81a27f1f684
Wagner Nogueira <arquitetowagnernogueira@gmail.com>
2023-06-04 11:17:50 -0400

    Pagina de estatísticas

--- GestaoDDD.MVC/Scripts/GoogleMaps/Estatisticas/grafico.js
+++ GestaoDDD.MVC/Scripts/GoogleMaps/Estatisticas/grafico.js
@@ -1,19 +1,74 @@
-﻿var estatisticas = [
-    { Nome: "Categoria 1", Valor: 10 },
-    { Nome: "Categoria 2", Valor: 20 },
-    { Nome: "Categoria 3", Valor: 15 },
-    { Nome: "Categoria 4", Valor: 25 },
-    { Nome: "Categoria 5", Valor: 18 }
-];
+﻿// Use o evento 'DOMContentLoaded' para aguardar o carregamento completo do documento
+document.addEventListener("DOMContentLoaded", function () {
+    google.charts.load("current", { packages: ["corechart"] });
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
 
-var chartContainer = document.getElementById("chartContainer");
+        /*Metropolitana */
+        var data_metro = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Crimes Violentos Letais Intencionais', 28],
+            ['Crimes Contra o Patrimônio', 6674],
+            ['Outros Crimes', 1089],
+            ['Produtividade Policial', 1961]
+        ]);
 
-for (var i = 0; i < estatisticas.length; i++) {
-    var linha = document.createElement("div");
-    linha.className = "chart-line";
-    linha.style.width = "calc(100% / " + estatisticas.length + ")";
-    linha.style.left = "calc(" + (i * 100) + "% / " + estatisticas.length + ")";
-    linha.style.backgroundSize = "100% " + estatisticas[i].Valor + "%";
-    linha.setAttribute("data-tooltip", estatisticas[i].Nome + ": " + estatisticas[i].Valor);
-    chartContainer.appendChild(linha);
-}
+        var options_metro = {
+            title: 'BALANÇO CRIMINAL \nCOMPARATIVO MENSAL 2022 - POR NATUREZA',
+            is3D: true,
+        };
+
+        /* Sul */
+        var data_sul = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Crimes Violentos Letais Intencionais', 63],
+            ['Crimes Contra o Patrimônio', 4471],
+            ['Outros Crimes', 551],
+            ['Produtividade Policial', 1840]
+        ]);
+
+        var options_sul = {
+            title: 'BALANÇO CRIMINAL \nCOMPARATIVO MENSAL 2022 - POR NATUREZA',
+            is3D: true,
+        };
+
+        /* Leste */
+        var data_leste = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Crimes Violentos Letais Intencionais', 85],
+            ['Crimes Contra o Patrimônio', 3977],
+            ['Outros Crimes', 733],
+            ['Produtividade Policial', 2166]
+        ]);
+
+        var options_leste = {
+            title: 'BALANÇO CRIMINAL \nCOMPARATIVO MENSAL 2022 - POR NATUREZA',
+            is3D: true,
+        };
+
+        /* Oeste */
+        var data_oeste = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Crimes Violentos Letais Intencionais', 109],
+            ['Crimes Contra o Patrimônio', 12442],
+            ['Outros Crimes', 1484],
+            ['Produtividade Policial', 4710]
+        ]);
+
+        var options_oeste = {
+            title: 'BALANÇO CRIMINAL \nCOMPARATIVO MENSAL 2022 - POR NATUREZA',
+            is3D: true,
+        };
+
+        /* setando dados */
+        var chartNorte = new google.visualization.PieChart(document.getElementById('piechart_norte'));
+        var chartSul = new google.visualization.PieChart(document.getElementById('piechart_sul'));
+        var chartLeste = new google.visualization.PieChart(document.getElementById('piechart_leste'));
+        var chartOeste = new google.visualization.PieChart(document.getElementById('piechart_oeste'));
+
+        chartNorte.draw(data_metro, options_metro);
+        chartSul.draw(data_sul, options_sul);
+        chartLeste.draw(data_leste, options_leste);
+        chartOeste.draw(data_oeste, options_oeste);
+    }
+});
