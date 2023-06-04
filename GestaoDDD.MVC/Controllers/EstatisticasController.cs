8e3650e8372423dedc5337aee30021dde939b5b1
Wagner Nogueira <arquitetowagnernogueira@gmail.com>
2023-06-04 19:36:18 -0400

    novos gráficos

---
+++ GestaoDDD.MVC/Controllers/EstatisticasController.cs
@@ -0,0 +1,100 @@
+﻿using GestaoDDD.Application.Interface;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace GestaoDDD.MVC.Controllers
+{
+  public class EstatisticasController : Controller
+  {
+    private readonly IDptoPoliciaAppService _dptoPoliciaAppService;
+    private readonly IAlunoAppService _alunoAppService;
+    private readonly IDptoAlunoAppService _dptoAlunoAppService;
+
+    public EstatisticasController(IDptoPoliciaAppService dptoPoliciaAppService,
+      IAlunoAppService alunoAppService,
+      IDptoAlunoAppService dptoAlunoAppService
+      )
+    {
+      _dptoPoliciaAppService = dptoPoliciaAppService;
+      _alunoAppService = alunoAppService;
+      _dptoAlunoAppService = dptoAlunoAppService;
+    }
+
+    public ActionResult Estatisticas()
+    {
+      return View();
+    }
+
+    public ActionResult EstatisticasDF()
+    {
+      return PartialView();
+    }
+
+    public JsonResult PegarDelegaciasPessoas()
+    {
+      var dp = new DelegaciasPessoas();
+      var lista = dp.Pegar();
+      return Json(lista, JsonRequestBehavior.AllowGet);
+    }
+  }
+
+  public class DelegaciasPessoas
+  {
+    public string Nome { get; set; }
+    public int Vagas { get; set; }
+    public int Pessoas { get; set; }
+
+    public DelegaciasPessoas()
+    {
+
+    }
+
+    public DelegaciasPessoas(string pNome, int vagas, int pessoas)
+    {
+      var rnd = new Random();
+      Nome = pNome;
+      Vagas = vagas;
+      Pessoas = pessoas;
+    }
+
+    public List<DelegaciasPessoas> Pegar()
+    {
+      var list = new List<DelegaciasPessoas>();
+      var rnd = new Random();
+      
+      list.Add(new DelegaciasPessoas("01ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("02ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("03ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("04ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("05ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("06ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("07ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("08ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("09ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("10ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("11ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("12ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("13ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("14ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("15ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("16ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("17ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("18ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("19ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("20ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("21ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("22ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("23ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("24ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("25ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("26ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("27ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("28ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      list.Add(new DelegaciasPessoas("29ª DP", rnd.Next(50, 200), rnd.Next(20, 150)));
+      return list;
+
+    }
+  }
+}
---
+++ GestaoDDD.MVC/Scripts/GoogleMaps/Estatisticas/grafico-delegacias.js
@@ -0,0 +1,124 @@
+﻿// Use o evento 'DOMContentLoaded' para aguardar o carregamento completo do documento
+function carregarDelegacias() {
+    fetch('/Estatisticas/EstatisticasDF', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        cache: 'no-cache',
+    })
+        .then(response => response.text())
+        .then(data => {
+
+            var dataTableArray = [['Delegacias', 'Qtd Vagas', 'Qtd Pessoas Escolheu']];
+            data.forEach(function (item) {
+                var linha = [item.Nome, item.QtdVagas, item.QtdPessoasEscolheu];
+                dataTableArray.push(linha);
+            });
+
+            var options = {
+                chart: {
+                    title: 'Delegacias mais procuradas durante o CFP.',
+                    subtitle: 'Qtd de vagas (Vagas) x Qtd de pessoas (Pessoas) que desejam trabalhar nessa unidade',
+                }
+            };
+
+            var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+            chart.draw(data, google.charts.Bar.convertOptions(options));
+
+        })
+        .catch(error => {
+            // Se houver algum erro na requisição, ele será capturado aqui
+            console.error('Erro:', error);
+        });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    google.charts.load('current', { 'packages': ['bar'] });
+    google.charts.setOnLoadCallback(draw_servico);
+
+
+    var vbDelegaciasPessoas = document.getElementById("vbDelegaciasPessoas").value;
+
+    //carregarDelegacias(vbDelegaciasPessoas);
+
+    //function drawChart_coluna() {
+    //    var data = google.visualization.arrayToDataTable([
+    //        ['Delegacias', 'Qtd Vagas', 'Qtd Pessoas Escolheu'],
+    //        ['2014', 1000, 400],
+    //        ['2015', 1170, 460],
+    //        ['2016', 660, 1120],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['2017', 1030, 540],
+    //        ['32ª', 1030, 540]
+    //    ]);
+
+    //    var options = {
+    //        chart: {
+    //            title: 'Delegacias mais procuradas durante o CFP.',
+    //            subtitle: 'Qtd de vagas (Vagas) x Qtd de pessoas (Pessoas) que desejam trabalhar nessa unidade',
+    //        }
+    //    };
+
+    //    var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+    //    chart.draw(data, google.charts.Bar.convertOptions(options));
+    //}
+
+    function draw_servico() {
+        fetch('/Estatisticas/PegarDelegaciasPessoas', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            cache: 'no-cache',
+        })
+            .then(response => response.text())
+            .then(data => {
+
+                var jsonData = JSON.parse(data);
+
+                var dataTable = new google.visualization.DataTable();
+                dataTable.addColumn('string', 'Delegacias');
+                dataTable.addColumn('number', 'Qtd Vagas');
+                dataTable.addColumn('number', 'Qtd Pessoas Escolheu');
+
+                var dataTableArray = [['Delegacias', 'Qtd Vagas', 'Qtd Pessoas Escolheu']];
+                jsonData.forEach(function (item) {
+                    dataTable.addRow([item.Nome, item.Vagas, item.Pessoas]);
+                });
+
+                var options = {
+                    chart: {
+                        title: 'Delegacias mais procuradas durante o CFP.',
+                        subtitle: 'Qtd de vagas (Vagas) x Qtd de pessoas (Pessoas) que desejam trabalhar nessa unidade',
+                    }
+                };
+
+                var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+                chart.draw(dataTable, google.charts.Bar.convertOptions(options));
+
+            })
+            .catch(error => {
+                // Se houver algum erro na requisição, ele será capturado aqui
+                console.error('Erro:', error);
+            });
+    }
+
+
+});
