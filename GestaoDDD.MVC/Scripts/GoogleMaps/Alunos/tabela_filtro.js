56bf0e79552042d8d865c1e1e96437be3ae93264
Wagner Nogueira <arquitetowagnernogueira@gmail.com>
2023-08-29 14:23:29 -0300

    Ajustando js

--- GestaoDDD.MVC/Scripts/GoogleMaps/Alunos/tabela_filtro.js
+++ GestaoDDD.MVC/Scripts/GoogleMaps/Alunos/tabela_filtro.js
@@ -111,7 +111,7 @@ function PesquisarAlunosPorPalavras(pTermo) {
             parsedData.forEach(function (item) {
                 var newRow = $("<tr class='table-row'>")
                     .attr("data-url", "/Aluno/PainelAluno?pAlunoId=" + item.ID)
-                    .append($("<td>").text(item.Posicao))
+                    .append($("<td>").text(item.PosicaoProvisoria))
                     .append($("<td>").text(item.Inscricao))
                     .append($("<td>").text(item.Nome))
                     .append($("<td>").text(item.NotaEtapa1))
