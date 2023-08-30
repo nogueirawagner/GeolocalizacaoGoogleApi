74f2110e7440bd3946b2caec914ae9b9db0e0379
Wagner Nogueira <arquitetowagnernogueira@gmail.com>
2023-08-29 22:35:55 -0300

    removendo dependencias de owin

--- GestaoDDD.MVC/App_Start/Startup.Auth.cs
+++ GestaoDDD.MVC/App_Start/Startup.Auth.cs
@@ -1,115 +1,104 @@
-﻿using GestaoDDD.Infra.Identity.Configuration;
-using GestaoDDD.Infra.Identity.Model;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin;
-using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.DataProtection;
-using Owin;
-using System;
-using System.Web.Mvc;
-using Microsoft.Owin.Security.Facebook;
-using System.Threading.Tasks;
-using Autofac;
+﻿using Microsoft.Owin.Security.DataProtection;
 
 namespace GestaoDDD.MVC
 {
-	public partial class Startup
+  public partial class Startup
 	{
         private const string XmlSchemaString = "http://www.w3.org/2001/XMLSchema#string";
         public static IDataProtectionProvider DataProtectionProvider { get; set; }
-        public void ConfigureAuth(IAppBuilder app)
-        {
 
-            DataProtectionProvider = app.GetDataProtectionProvider();
+        //public void ConfigureAuth(IAppBuilder app)
+        //{
 
-            var builder = new ContainerBuilder();
+        //    DataProtectionProvider = app.GetDataProtectionProvider();
 
-            builder.Register<IDataProtectionProvider>(c => DataProtectionProvider)
-                    .InstancePerLifetimeScope();
+        //    var builder = new ContainerBuilder();
 
-            // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(() => DependencyResolver.Current.GetService<ApplicationUserManager>());
-            app.CreatePerOwinContext(() => DependencyResolver.Current.GetService<ApplicationSignInManager>());
-            app.CreatePerOwinContext(() => DependencyResolver.Current.GetService<ApplicationRoleManager>());
+        //    builder.Register<IDataProtectionProvider>(c => DataProtectionProvider)
+        //            .InstancePerLifetimeScope();
 
-            // Enable the application to use a cookie to store information for the signed in user
-            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
-            // Configure the sign in cookie
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login"),
-                Provider = new CookieAuthenticationProvider
-                {
-                    // Enables the application to validate the security stamp when the user logs in.
-                    // This is a security feature which is used when you change a password or add an external login to your account.  
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                        validateInterval: TimeSpan.FromMinutes(5),
-                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
-                }
-            });
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+        //    // Configure the db context, user manager and signin manager to use a single instance per request
+        //    app.CreatePerOwinContext(() => DependencyResolver.Current.GetService<ApplicationUserManager>());
+        //    app.CreatePerOwinContext(() => DependencyResolver.Current.GetService<ApplicationSignInManager>());
+        //    app.CreatePerOwinContext(() => DependencyResolver.Current.GetService<ApplicationRoleManager>());
 
-            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
-            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+        //    // Enable the application to use a cookie to store information for the signed in user
+        //    // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+        //    // Configure the sign in cookie
+        //    app.UseCookieAuthentication(new CookieAuthenticationOptions
+        //    {
+        //        AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+        //        LoginPath = new PathString("/Account/Login"),
+        //        Provider = new CookieAuthenticationProvider
+        //        {
+        //            // Enables the application to validate the security stamp when the user logs in.
+        //            // This is a security feature which is used when you change a password or add an external login to your account.  
+        //            OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+        //                validateInterval: TimeSpan.FromMinutes(5),
+        //                regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+        //        }
+        //    });
+        //    app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
-            // Enables the application to remember the second login verification factor such as phone or email.
-            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
-            // This is similar to the RememberMe option when you log in.
-            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+        //    // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
+        //    app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
 
-            // Uncomment the following lines to enable logging in with third party login providers
+        //    // Enables the application to remember the second login verification factor such as phone or email.
+        //    // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
+        //    // This is similar to the RememberMe option when you log in.
+        //    app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "MYCLIENTID",
-            //    clientSecret: "MYCLIENTID");
+        //    // Uncomment the following lines to enable logging in with third party login providers
 
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "MYCLIENTID",
-            //   consumerSecret: "MYCLIENTID");
+        //    //app.UseMicrosoftAccountAuthentication(
+        //    //    clientId: "MYCLIENTID",
+        //    //    clientSecret: "MYCLIENTID");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "1213099755371227",
-            //   appSecret: "67c91421c354cdd6272f3c96765d8d53");
+        //    //app.UseTwitterAuthentication(
+        //    //   consumerKey: "MYCLIENTID",
+        //    //   consumerSecret: "MYCLIENTID");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "103842386017689616225",
-            //    ClientSecret = "fc88974c3ea2abc0df457963b7240f4e844e46bd"
-            //});
+        //    //app.UseFacebookAuthentication(
+        //    //   appId: "1213099755371227",
+        //    //   appSecret: "67c91421c354cdd6272f3c96765d8d53");
 
-            var fao = new FacebookAuthenticationOptions
-            {
-                AppId = "1213099755371227",
-                AppSecret = "67c91421c354cdd6272f3c96765d8d53"
-            };
+        //    //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+        //    //{
+        //    //    ClientId = "103842386017689616225",
+        //    //    ClientSecret = "fc88974c3ea2abc0df457963b7240f4e844e46bd"
+        //    //});
 
-            fao.Scope.Add("email");
-            fao.Scope.Add("user_friends");
-            fao.Scope.Add("public_profile");
+        //    var fao = new FacebookAuthenticationOptions
+        //    {
+        //        AppId = "1213099755371227",
+        //        AppSecret = "67c91421c354cdd6272f3c96765d8d53"
+        //    };
 
-            fao.Provider = new FacebookAuthenticationProvider()
-            {
+        //    fao.Scope.Add("email");
+        //    fao.Scope.Add("user_friends");
+        //    fao.Scope.Add("public_profile");
 
-                OnAuthenticated = (context) =>
-                {
-                    context.Identity.AddClaim(new System.Security.Claims.Claim("urn:facebook:access_token", context.AccessToken, XmlSchemaString, "Facebook"));
-                    foreach (var x in context.User)
-                    {
-                        var claimType = string.Format("urn:facebook:{0}", x.Key);
-                        string claimValue = x.Value.ToString();
-                        if (!context.Identity.HasClaim(claimType, claimValue))
-                            context.Identity.AddClaim(new System.Security.Claims.Claim(claimType, claimValue, XmlSchemaString, "Facebook"));
+        //    fao.Provider = new FacebookAuthenticationProvider()
+        //    {
 
-                    }
-                    return Task.FromResult(0);
-                }
-            };
+        //        OnAuthenticated = (context) =>
+        //        {
+        //            context.Identity.AddClaim(new System.Security.Claims.Claim("urn:facebook:access_token", context.AccessToken, XmlSchemaString, "Facebook"));
+        //            foreach (var x in context.User)
+        //            {
+        //                var claimType = string.Format("urn:facebook:{0}", x.Key);
+        //                string claimValue = x.Value.ToString();
+        //                if (!context.Identity.HasClaim(claimType, claimValue))
+        //                    context.Identity.AddClaim(new System.Security.Claims.Claim(claimType, claimValue, XmlSchemaString, "Facebook"));
 
-            fao.SignInAsAuthenticationType = DefaultAuthenticationTypes.ExternalCookie;
-            app.UseFacebookAuthentication(fao); 
-        }
+        //            }
+        //            return Task.FromResult(0);
+        //        }
+        //    };
+
+        //    fao.SignInAsAuthenticationType = DefaultAuthenticationTypes.ExternalCookie;
+        //    app.UseFacebookAuthentication(fao); 
+        //}
 
 
 	}
